a9b4d6c77c037223f96d568b29e2001de707bafd
Arthur Viviere <arthur.viviere@grenoble-inp.fr>
2022-01-22 18:23:14 +0100

    delete

--- build/CMakeFiles/nbody.dir/cmake_clean.cmake
+++ build/CMakeFiles/nbody.dir/cmake_clean.cmake
@@ -1,11 +1,16 @@
 file(REMOVE_RECURSE
   "CMakeFiles/nbody.dir/src/Body.cpp.o"
+  "CMakeFiles/nbody.dir/src/Body.cpp.o.d"
   "CMakeFiles/nbody.dir/src/NBody.cpp.o"
-  "CMakeFiles/nbody.dir/src/Physics.cpp.o"
+  "CMakeFiles/nbody.dir/src/NBody.cpp.o.d"
   "CMakeFiles/nbody.dir/src/Vec3.cpp.o"
+  "CMakeFiles/nbody.dir/src/Vec3.cpp.o.d"
   "CMakeFiles/nbody.dir/src/Viewer.cpp.o"
+  "CMakeFiles/nbody.dir/src/Viewer.cpp.o.d"
   "CMakeFiles/nbody.dir/src/main.cpp.o"
+  "CMakeFiles/nbody.dir/src/main.cpp.o.d"
   "CMakeFiles/nbody.dir/src/random_init.cpp.o"
+  "CMakeFiles/nbody.dir/src/random_init.cpp.o.d"
   "nbody"
   "nbody.pdb"
 )
